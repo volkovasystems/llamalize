dcb2ec64039486f4f8ed23253ba2362230571450
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-02 20:44:31 +0800

    Update client/server code format.

--- test.module.js
+++ test.module.js
@@ -221,23 +221,3 @@ describe( "llamalize", ( ) => {
 } );
 
 //: @end-bridge
-
-
-
-
-
-
-// const assert = require( "assert" );
-// const llamalize = require( "./llamalize.js" );
-
-// assert.equal( llamalize( "hello-world" ), "helloWorld", "should have value 'helloWorld'" );
-
-// assert.equal( llamalize( "hello_world" ), "helloWorld", "should have value 'helloWorld'" );
-
-// assert.equal( llamalize( "helloWorld" ), "helloWorld", "should have value 'helloWorld'" );
-
-// assert.equal( llamalize( "hello world" ), "helloWorld", "should have value 'helloWorld'" );
-
-// assert.equal( llamalize( "hello world", true ), "HelloWorld", "should have value 'HelloWorld'" );
-
-// console.log( "ok" );
