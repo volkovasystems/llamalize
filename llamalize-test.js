2df6f8e2830d7052f121ddb5950494b4a6711956
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-09 13:19:18 +0800

    Updated test.

--- llamalize-test.js
+++ llamalize-test.js
@@ -2,14 +2,14 @@
 const assert = require( "assert" );
 const llamalize = require( "./llamalize.js" );
 
-assert.equal( llamalize( "hello-world" ), "helloWorld", "should have value 'helloWorld'" );
+assert.equal( llamalize( "hello-world" ), "helloWorld", "should be equal to 'helloWorld'" );
 
-assert.equal( llamalize( "hello_world" ), "helloWorld", "should have value 'helloWorld'" );
+assert.equal( llamalize( "hello_world" ), "helloWorld", "should be equal to 'helloWorld'" );
 
-assert.equal( llamalize( "helloWorld" ), "helloWorld", "should have value 'helloWorld'" );
+assert.equal( llamalize( "helloWorld" ), "helloWorld", "should be equal to 'helloWorld'" );
 
-assert.equal( llamalize( "hello world" ), "helloWorld", "should have value 'helloWorld'" );
+assert.equal( llamalize( "hello world" ), "helloWorld", "should be equal to 'helloWorld'" );
 
-assert.equal( llamalize( "hello world", true ), "HelloWorld", "should have value 'HelloWorld'" );
+assert.equal( llamalize( "hello world", true ), "HelloWorld", "should be equal to 'HelloWorld'" );
 
 console.log( "ok" );
